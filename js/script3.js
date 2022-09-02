3ab91981284c504cb75b959b69164ee0c2c13c8d
Matheus Souza <matheus3864@gmail.com>
2022-09-02 12:42:10 -0300

    Elaboration scripts for page3 and page4

--- js/script3.js
+++ js/script3.js
@@ -1,4 +1,32 @@
-function click(){
-    var a = document.getElementById('area')
-    a.innerText = 'Clicked!'
+// PAGE 2
+function clicar(){
+    let a = document.getElementById('area')
+    a.addEventListener('onclick', clicar)
+    a.innerText = "Clicked!"
+    a.style.background = "rgba(178,192,248,1.1)"
 }
+
+function enter(){
+    let a = document.getElementById('area')
+    a.addEventListener('onmouseenter', enter)
+    a.innerText = 'In!'
+}
+
+function leave(){
+    let a = document.getElementById('area')
+    a.addEventListener('onmouseleave', leave)
+    a.innerText = 'Out!'
+}
+
+
+// PAGE 3
+function somar(){
+    let tn1 = parseInt(document.getElementById('txtn1').value)
+    let tn2 = parseInt(document.getElementById('txtn2').value)
+    let s = tn1 + tn2
+
+    let result = document.getElementById('res')
+    result.innerHTML = `Result: ${s}`
+}
+
+// 157.245.249.144
