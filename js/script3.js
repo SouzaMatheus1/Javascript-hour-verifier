e2d2a67352b6d45e28be962e39388e4e64e14615
Matheus Souza <matheus3864@gmail.com>
2022-09-15 14:35:23 -0300

    Deleted

--- js/script3.js
+++ js/script3.js
@@ -109,15 +109,15 @@ let minute = date.getMinutes()<10?'0':date.getMinutes()
 msg.innerHTML = `It's ${hour}:${minute}`
 
 if (hour >= 0 && hour < 12){
-    img.src = 'images/manha.jpg'
+    img.src = 'images/manha.png'
     document.body.style.background = '#B47681' // rgb(180,118,129)
     foot.style.background = '#B476AD'
 }else if ( hour >= 12 && hour < 18){
-    img.src = 'images/tarde.jpg'
+    img.src = 'images/tarde.png'
     document.body.style.background = '#FF9D01' // rgb(255,157,1)
     foot.style.background = '#FF7B01'
 }else{
-    img.src = 'images/noite.jpg'
+    img.src = 'images/noite.png'
     document.body.style.background = '#053364' // rgb(5,51,100)
     foot.style.background = '#05334E'
     reg.style.color = '#FFFFFF'
