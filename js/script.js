f575ad361653aec81fbe5583d08a6099c9270203
Matheus Souza <matheus3864@gmail.com>
2022-09-15 17:54:41 -0300

    Att js script

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
-// var n1 = parseInt(window.prompt("Numero 1:"))
-// var n2 = parseInt(window.prompt("Numero 2:"))
-var nome = String(window.prompt("Nome: "))
-document.writeln(`Seu nome tem ${nome.length} letras.`)
-// var soma = n1 + n2
-// window.alert(String(`A soma dos números ${n1} e ${n2} é ${soma}`))
-document.write(`${nome.toUpperCase()}`)
-document.write(`${nome.toLowerCase()}`)
-var s = 'Javascript'
-
-s.length
-s.toUpperCase()
-s.toLowerCase()
-
-var n1 = 123.4
-n1.toFixed(2) // Numeros depois da virgula
-n1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) // Para valor monetario real
-
-s == 'Javascript' ? alert('oi') : alert('não')
-
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
+function charge(){
+    let msg = document.getElementById('msg')
+    let img = document.getElementById('img')
+    let foot = document.querySelector('footer#feet')
+    let reg = document.querySelector('p#reg')
+    let date = new Date()
+    let hour = date.getHours()
+    let minute = date.getMinutes()<10?'0':date.getMinutes()
+    
+    msg.innerHTML = `It's ${hour}:${minute}`
+    
+    if (hour >= 0 && hour < 12){
+        img.src = 'images/manha.png'
+        document.body.style.background = '#B47681' // rgb(180,118,129)
+        foot.style.background = '#B476AD'
+    }else if ( hour >= 12 && hour < 18){
+        img.src = 'images/tarde.png'
+        document.body.style.background = '#FF9D01' // rgb(255,157,1)
+        foot.style.background = '#FF7B01'
+    }else{
+        img.src = 'images/noite.png'
+        document.body.style.background = '#053364' // rgb(5,51,100)
+        foot.style.background = '#05334E'
+        reg.style.color = '#FFFFFF'
+    }
+    }
